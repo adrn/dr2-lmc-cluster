36611b2bd4ece0e4a0a1912753ebd94c3e004b03
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-10-12 15:24:48 -0400

    update script

--- mpi/starmodel.sh
+++ mpi/starmodel.sh
@@ -2,7 +2,8 @@
 #SBATCH -J isochrones                 # job name
 #SBATCH -o isochrones.o%j             # output file name (%j expands to jobID)
 #SBATCH -e isochrones.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 112
+#SBATCH -n 160                        # number of cores (not nodes!)
+#SBATCH -p cca                        # add to the CCA queue
 #SBATCH -t 12:00:00                   # run time (hh:mm:ss)
 
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/home/apricewhelan/software/lib/
@@ -12,6 +13,6 @@ module load gcc openmpi2
 
 date
 
-srun python sample-stellar-params.py --mpi --ncores=112
+srun python sample-stellar-params.py --mpi --ncores=160
 
 date

ff1371259a1e763b6a9a28ccaf8a49f7876fe62a
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-10-12 15:04:18 -0400

    submit script

--- mpi/starmodel.sh
+++ mpi/starmodel.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#SBATCH -J starmodel                 # job name
-#SBATCH -o starmodel.o%j             # output file name (%j expands to jobID)
-#SBATCH -e starmodel.e%j             # error file name (%j expands to jobID)
+#SBATCH -J isochrones                 # job name
+#SBATCH -o isochrones.o%j             # output file name (%j expands to jobID)
+#SBATCH -e isochrones.e%j             # error file name (%j expands to jobID)
 #SBATCH -n 112
-#SBATCH -t 12:00:00             # run time (hh:mm:ss)
+#SBATCH -t 12:00:00                   # run time (hh:mm:ss)
 
 cd
 
 module load gcc openmpi2
 
-source activate scf
-
 date
 
-srun python field-of-streams.py -f ../../data/cosmohub-b5.fits -v --nworkers=1024 --mpi
+srun python sample-stellar-params.py --mpi --ncores=112
 
 date

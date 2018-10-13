3e3821a01d779974d603b8c36cf0754500d3c67d
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-10-13 10:51:08 -0400

    try emcee instead

--- scripts/sample-stellar-params.py
+++ scripts/sample-stellar-params.py
@@ -34,23 +34,27 @@ def worker(task):
     iso = MIST_Isochrone(['DECam_u',
                           'DECam_g',
                           'DECam_i'])
+    nwalkers = 300 # MAGIC NUMBER
 
     for i in np.arange(i1, i2+1, dtype=int):
         row = decam[i]
         name = 'lmcla-{0}-'.format(row['index'])
         model_file = 'chains/starmodels-{0}.hdf5'.format(str(row['index']))
-        
+
         if path.exists(model_file) and not overwrite:
             print('skipping {0} - already exists'.format(name))
             continue
-        
+
         model = StarModel(ic=iso,
                           DECam_g=(row['GMAG'], 2 * row['GERR']),
                           DECam_i=(row['IMAG'], 2 * row['IERR'])) # MAGIC NUMBER
 
         print('sampling star {0}'.format(row['index']))
         model.set_bounds(distance=(1000., 50000.))
-        model.fit_multinest(basename=name, overwrite=overwrite)
+        # model.fit_multinest(basename=name, overwrite=overwrite)
+        model.fit_mcmc(nwalkers=nwalkers,
+                       p0=np.array([350., 8., -0.5, 30000., 0.1]),
+                       nburn=512, niter=2048)
         model.save_hdf(model_file)
 
         fig = model.corner_physical()

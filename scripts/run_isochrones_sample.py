056c30aa9b1679ea369b2a611280197f1989580f
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-10-13 17:52:48 -0400

    use emcee false

--- scripts/run_isochrones_sample.py
+++ scripts/run_isochrones_sample.py
@@ -46,7 +46,7 @@ def main(index, overwrite=False):
     model = StarModel(ic=iso,
                       DECam_g=(row['GMAG'], 2 * row['GERR']), # MAGIC NUMBER
                       DECam_i=(row['IMAG'], 2 * row['IERR']), # MAGIC NUMBER
-                      use_emcee=True)
+                     )
 
     print('sampling star {0}'.format(row['index']))
     model.set_bounds(distance=(1000., 50000.))
